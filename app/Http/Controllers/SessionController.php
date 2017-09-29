9459d3695bbb330d93a21a95e5b351938bbfab18
Nobita <lwx863503914@gmail.com>
2017-09-29 19:05:13 +0000

    Finish user edit, update, index, and destroy actions

--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -8,6 +8,15 @@ use Auth;
 
 class SessionController extends Controller
 {
+    public function __construct()
+    {
+        //只让未登录用户访问登录页面
+        //使用 Auth 中间件提供的 guest 选项，用于指定一些只允许未登录用户访问的动作
+        $this->middleware('guest',[
+            'only' => ['create'],
+        ]);
+    }
+
     public function create()
     {
     	return view('sessions.create');
@@ -27,13 +36,12 @@ class SessionController extends Controller
 
     	if (Auth::attempt($credentials,$request->has('remember'))) {
     		session()->flash('success','欢迎回来!');
-    		return redirect()->route('users.show',[Auth::user()]);
+    		return redirect()->intended(route('users.show',[Auth::user()]));
     	}else{
     		session()->flash('danger','很抱歉，您的邮箱和密码不匹配');
     		return redirect()->back();
     	}
 
-    	return;
     }
 
     public function destroy()
