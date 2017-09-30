28b4fb538a292d29f82864b77fee80f18ed55284
Nobita <lwx863503914@gmail.com>
2017-09-30 08:00:38 +0000

    Add account activations

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -6,13 +6,14 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Models\User;
 use Auth;
+use Mail;
 
 class UsersController extends Controller
 {
 	public function __construct()
 	{
 		$this->middleware('auth',[
-			'except' => ['show','store','create','index']//除了这些都过滤
+			'except' => ['show','store','create','index','confirmEmail']//除了这些都过滤
 		]);
 
 		//只让未登录用户访问注册页面
@@ -45,8 +46,33 @@ class UsersController extends Controller
     		'email' => $request->email,
     		'password' => bcrypt($request->password),
     	]);
+    	$this->sendEmailConfirmationTo($user);
+    	session()->flash('success','验证邮件已发送到你的注册邮箱上，请注意查收。');
+    	return redirect('/');
+    }
+
+    public function sendEmailConfirmationTo($user)
+    {
+    	$view = 'emails.confirm';
+    	$data = compact('user');
+    	$from = 'nobitalu@163.com';
+    	$name = 'NobitLu';
+    	$to = $user->email;
+    	$subject = '感谢注册 Sample 应用！请确认你的邮箱。';
+    	Mail::send($view, $data, function ($message) use ($from, $name, $to, $subject) {
+            $message->from($from, $name)->to($to)->subject($subject);
+        });
+    }
+
+    public function confirmEmail($token)
+    {
+    	$user = User::where('activation_token',$token)->firstOrFail();
+    	$user->activated  = true;
+    	$user->activation_token = null;
+    	$user->save();
+
     	Auth::login($user);
-    	session()->flash('success','欢迎，您将在这里开启一段新的旅程~');
+    	session()->flash('success','恭喜你，激活成功！');
     	return redirect()->route('users.show',[$user]);
     }
 
